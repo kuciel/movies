e936086bda6243e050f93384eb498bf3d3dd45a2
Andrzej Kuciel <Andrzej.Kuciel@sigma.se>
2021-12-02 10:24:08 +0100

    Initial scafolding

---
+++ app/src/main/java/com/andysworkshop/movies/popularmoviesscreen/PopularMoviesFragment.kt
@@ -0,0 +1,45 @@
+package com.andysworkshop.movies.popularmoviesscreen
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.andysworkshop.movies.R
+import com.andysworkshop.movies.databinding.FragmentPopularMoviesBinding
+
+/**
+ * A simple [Fragment] subclass as the default destination in the navigation.
+ */
+class PopularMoviesFragment : Fragment() {
+
+    private var _binding: FragmentPopularMoviesBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        _binding = FragmentPopularMoviesBinding.inflate(inflater, container, false)
+        return binding.root
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.buttonFirst.setOnClickListener {
+            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
