bb42429a883fcf9a5d59939c7545f87bda89b8ae
Andrzej Kuciel <Andrzej.Kuciel@sigma.se>
2021-12-09 17:27:49 +0100

    Add image placeholder to make scrolling smoother

--- app/src/main/java/com/andysworkshop/movies/popularmoviesscreen/MoviesRecyclerAdapter.kt
+++ app/src/main/java/com/andysworkshop/movies/popularmoviesscreen/MoviesRecyclerAdapter.kt
@@ -2,7 +2,9 @@ package com.andysworkshop.movies.popularmoviesscreen
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.appcompat.content.res.AppCompatResources
 import androidx.recyclerview.widget.RecyclerView
+import com.andysworkshop.movies.R
 import com.andysworkshop.movies.databinding.PosterViewBinding
 import com.andysworkshop.movies.popularmoviesscreen.data.PopularMoviesUIData
 import com.squareup.picasso.Picasso
@@ -23,7 +25,8 @@ class MoviesRecyclerAdapter(
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val item = values[position]
-        Picasso.get().load(item.posterPath).into(holder.posterImageView)
+        Picasso.get().load(item.posterPath).placeholder(R.color.cardview_dark_background)
+            .into(holder.posterImageView)
     }
 
     override fun getItemCount(): Int {
